7ccef3162cb9dbbe31f6fb1e95a2cfb7c5faf902
liza-mae <liza.dayoub@elastic.co>
2022-03-01 12:46:05 -0700

    try env test

---
+++ .buildkite/scripts/steps/estf/env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Set env variable in this step"
+
+export ESTF_ENV_TEST="liza"
+
---
+++ .buildkite/scripts/steps/estf/useenv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Does this env variable exists? "
+echo "${ESTF_ENV_TEST}"
+echo "end-- "
