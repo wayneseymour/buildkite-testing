63ef9989e6df8d46df3f81e31bd3f34bee6dedc9
liza-mae <liza.dayoub@elastic.co>
2022-03-21 13:20:57 -0600

    Debug

--- .buildkite/scripts/steps/estf/cloud/create_cloud_deployment.sh
+++ .buildkite/scripts/steps/estf/cloud/create_cloud_deployment.sh
@@ -7,12 +7,17 @@ source .buildkite/scripts/common/util.sh
 echo "Create cloud deployment"
 
 VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=estf-vault-role-id)"
+echo "after vault role id"
 VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=estf-vault-secret-id)"
+echo "after vault secret id"
 VAULT_TOKEN=$(retry 5 30 vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
+echo "after toek"
 retry 5 30 vault login -no-print "$VAULT_TOKEN"
+echo "after login"
 
 EC_API_KEY="$(vault kv get --field apiKey secret/stack-testing/estf-cloud)"
 export EC_API_KEY
+echo "ec key setup"
 
 ESTF_DEPLOYMENT_NAME="ESTF_Deployment_CI_$(uuidgen)"
 ESTF_PLAN_FILE="estf_cloud_plan.json"
