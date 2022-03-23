3c5026ea9431acbbf05f710b9ba5fd8c84df7426
liza-mae <liza.dayoub@elastic.co>
2022-03-23 13:30:27 -0600

    Testing env

--- .buildkite/scripts/steps/estf/cloud/create_cloud_deployment.sh
+++ .buildkite/scripts/steps/estf/cloud/create_cloud_deployment.sh
@@ -7,39 +7,32 @@ source .buildkite/scripts/common/util.sh
 echo "Create cloud deployment"
 
 VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=estf-vault-role-id)"
-echo "after vault role id"
 VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=estf-vault-secret-id)"
-echo "after vault secret id"
 VAULT_TOKEN=$(retry 5 30 vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
-echo "after toek"
 retry 5 30 vault login -no-print "$VAULT_TOKEN"
-echo "after login"
 
 EC_API_KEY="$(vault kv get --field apiKey secret/stack-testing/estf-cloud)"
 export EC_API_KEY
-echo "ec key setup"
 
 ESTF_DEPLOYMENT_NAME="ESTF_Deployment_CI_$(uuidgen)"
 ESTF_PLAN_FILE=".buildkite/scripts/steps/estf/cloud/estf_cloud_plan.json"
 OUTPUT_FILE=$(mktemp --suffix ".json")
 
-echo "INPUT"
-echo $ESTF_PLAN_FILE
-echo $ESTF_CLOUD_VERSION
-echo $(pwd)
-echo "END"
 ecctl deployment create --track --output json --name $ESTF_DEPLOYMENT_NAME \
                         --version $ESTF_CLOUD_VERSION --file $ESTF_PLAN_FILE &> "$OUTPUT_FILE"
 
-echo "after create"
 ESTF_DEPLOYMENT_ID=$(jq -sr '.[0].id' "$OUTPUT_FILE")
 ESTF_DEPLOYMENT_USERNAME=$(jq -sr '.[0].resources[0].credentials.username' "$OUTPUT_FILE")
 ESTF_DEPLOYMENT_PASSWORD=$(jq -sr '.[0].resources[0].credentials.password' "$OUTPUT_FILE")
+export ESTF_DEPLOYMENT_PASSWORD
 
-retry 5 15 vault write "secret/stack-testing/$ESTF_DEPLOYMENT_ID" username="$ESTF_DEPLOYMENT_USERNAME" password="$ESTF_DEPLOYMENT_PASSWORD"
+#retry 5 15 vault write "secret/stack-testing/$ESTF_DEPLOYMENT_ID" username="$ESTF_DEPLOYMENT_USERNAME" password="$ESTF_DEPLOYMENT_PASSWORD"
 
 ESTF_KIBANA_URL=$(ecctl deployment show "$ESTF_DEPLOYMENT_ID" --kind kibana | jq -r '.info.metadata.aliased_url')
+export ESTF_KIBANA_URL
+
 ESTF_ELASTICSEARCH_URL=$(ecctl deployment show "$ESTF_DEPLOYMENT_ID" --kind elasticsearch | jq -r '.info.metadata.aliased_url')
+export ESTF_ELASTICSEARCH_URL
 
 ESTF_KIBANA_HASH=$(curl -s -u "$ESTF_DEPLOYMENT_USERNAME:$ESTF_DEPLOYMENT_PASSWORD" $ESTF_KIBANA_URL/api/status | jq -r .version.build_hash)
 
--- .buildkite/scripts/steps/estf/cloud/shutdown_cloud_deployment.sh
+++ .buildkite/scripts/steps/estf/cloud/shutdown_cloud_deployment.sh
@@ -4,17 +4,26 @@ set -euo pipefail
 
 echo "Shutdown cloud deployment"
 
-VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=estf-vault-role-id)"
-VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=estf-vault-secret-id)"
-VAULT_TOKEN=$(retry 5 30 vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
-retry 5 30 vault login -no-print "$VAULT_TOKEN"
+if [ -z "$EC_API_KEY" ]; then
+  echo "ec api key is empty"
+fi
 
-EC_API_KEY="$(vault kv get --field apiKey secret/stack-testing/estf-cloud)"
-export EC_API_KEY
+if [ -z "$ESTF_ELASTICSEARCH_URL" ]; then
+  echo "es url is empty"
+fi
+
+#VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=estf-vault-role-id)"
+#VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=estf-vault-secret-id)"
+#VAULT_TOKEN=$(retry 5 30 vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
+#retry 5 30 vault login -no-print "$VAULT_TOKEN"
+
+#EC_API_KEY="$(vault kv get --field apiKey secret/stack-testing/estf-cloud)"
+#export EC_API_KEY
 
 buildkite-agent meta-data exists "estf-deployment-id"
 
 ESTF_DEPLOYMENT_ID=$(buildkite-agent meta-data get "estf-deployment-id")
 
-retry 2 15 vault delete "secret/stack-testing/$ESTF_DEPLOYMENT_ID"
 ecctl deployment shutdown --force $ESTF_DEPLOYMENT_ID
+
+#retry 2 15 vault delete "secret/stack-testing/$ESTF_DEPLOYMENT_ID"
