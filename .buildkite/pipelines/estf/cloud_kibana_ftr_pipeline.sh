08fbb57109ff9f49ad38bd8d3b32577b553460f4
liza-mae <liza.dayoub@elastic.co>
2022-04-28 16:46:35 -0600

    Fix echo

--- .buildkite/pipelines/estf/cloud_kibana_ftr_pipeline.sh
+++ .buildkite/pipelines/estf/cloud_kibana_ftr_pipeline.sh
@@ -7,31 +7,29 @@ echo "steps:"
 # Add kibana oss ci groups
 for i in `seq 1 2`;
 do
-  yml="  - group: \"Cloud Kibana Tests Group ${i}\"
-    key: \"cktg${i}\"
-    steps:
-      - label: \"Create Cloud Deployment\"
-        key: \"create_cloud_deployment${i}\"
-        command: .buildkite/scripts/steps/estf/cloud/create_cloud_deployment.sh
-        env:
-          BUILDKITE_GROUP_PARALLEL_JOB: ${i}
-        agents:
-          queue: n2-4
-      - label: \"Run Kibana Functional Tests\"
-        key: \"run_kibana_tests${i}\"
-        command: .buildkite/scripts/steps/estf/cloud/run_kibana_tests.sh
-        env:
-          BUILDKITE_GROUP_PARALLEL_JOB: ${i}
-        agents:
-          queue: n2-4
-        depends_on: \"create_cloud_deployment${i}\"
-      - wait: ~
-        continue_on_failure: true
-      - label: \"Delete Cloud Deployment\"
-        key: \"delete_cloud_deployment${i}\"
-        command: .buildkite/scripts/steps/estf/cloud/shutdown_cloud_deployment.sh
-        agents:
-          queue: n2-4
-  "
-  echo -n "$yml"
+  echo "  - group: \"Cloud Kibana Tests Group ${i}\""
+  echo "    key: \"cktg${i}\""
+  echo "    steps:"
+  echo "      - label: \"Create Cloud Deployment\""
+  echo "        key: \"create_cloud_deployment${i}\""
+  echo "        command: .buildkite/scripts/steps/estf/cloud/create_cloud_deployment.sh"
+  echo "        env:"
+  echo "          BUILDKITE_GROUP_PARALLEL_JOB: ${i}"
+  echo "        agents:"
+  echo "          queue: n2-4"
+  echo "      - label: \"Run Kibana Functional Tests\""
+  echo "        key: \"run_kibana_tests${i}\""
+  echo "        command: .buildkite/scripts/steps/estf/cloud/run_kibana_tests.sh"
+  echo "        env:"
+  echo "          BUILDKITE_GROUP_PARALLEL_JOB: ${i}"
+  echo "        agents:"
+  echo "          queue: n2-4"
+  echo "        depends_on: \"create_cloud_deployment${i}\""
+  echo "      - wait: ~"
+  echo "          continue_on_failure: true"
+  echo "      - label: \"Delete Cloud Deployment\""
+  echo "        key: \"delete_cloud_deployment${i}\""
+  echo "        command: .buildkite/scripts/steps/estf/cloud/shutdown_cloud_deployment.sh"
+  echo "        agents:"
+  echo "          queue: n2-4"
 done
