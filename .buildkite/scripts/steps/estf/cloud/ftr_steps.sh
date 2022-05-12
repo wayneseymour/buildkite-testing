31577a2753f426784c4db2a1676a2d0330e54c89
liza-mae <liza.dayoub@elastic.co>
2022-05-12 16:01:58 -0600

    Fix spacing

--- .buildkite/scripts/steps/estf/cloud/ftr_steps.sh
+++ .buildkite/scripts/steps/estf/cloud/ftr_steps.sh
@@ -14,8 +14,8 @@ group_count=$(jq -r '.count' ftr_run_order.json)
 echo "steps:"
 for i in $(seq -s ' ' 0 $((group_count-1))); do
   configs=$(jq -r ".groups[$i].names | .[]" ftr_run_order.json)
-  echo "- label: Test $i"
-  echo "  command: echo $configs"
-  echo "  agents:"
-  echo "    queue: kibana-default"
+  echo "  - label: Test $i"
+  echo "    command: echo $configs"
+  echo "    agents:"
+  echo "      queue: kibana-default"
 done
