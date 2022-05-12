3e7e580085ede28dc36366ffce1905f83c3f3d69
liza-mae <liza.dayoub@elastic.co>
2022-05-12 16:59:24 -0600

    Fix command

--- .buildkite/scripts/steps/estf/cloud/ftr_steps.sh
+++ .buildkite/scripts/steps/estf/cloud/ftr_steps.sh
@@ -11,7 +11,7 @@ set -eu
 buildkite-agent artifact download ftr_run_order.json .
 group_count=$(jq -r '.count' ftr_run_order.json)
 
-echo "  command: echo testing"
-echo "  parallelism: $group_count"
-echo "  agents:"
-echo "    queue: kibana-default"
+echo "  - command: echo testing"
+echo "    parallelism: $group_count"
+echo "    agents:"
+echo "      queue: kibana-default"
