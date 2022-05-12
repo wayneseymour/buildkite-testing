163923861bffe7e4ee296e6e53e1e59f41a6ed87
liza-mae <liza.dayoub@elastic.co>
2022-05-12 16:24:41 -0600

    Move upload

--- .buildkite/pipelines/estf/pick_test_group_run_order.sh
+++ .buildkite/pipelines/estf/pick_test_group_run_order.sh
@@ -13,6 +13,3 @@ source .buildkite/scripts/common/env.sh
 
 echo '--- Pick Test Group Run Order'
 node "$(dirname "${0}")/pick_test_group_run_order.js" || true
-
-echo '-- Upload test steps'
-.buildkite/scripts/steps/estf/cloud/ftr_steps.sh | buildkite-agent pipeline upload
