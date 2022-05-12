5e6abab142830319839dfe1132be9a2cae35038a
liza-mae <liza.dayoub@elastic.co>
2022-05-12 15:54:26 -0600

    Fix permissions

--- .buildkite/pipelines/estf/pick_test_group_run_order.sh
+++ .buildkite/pipelines/estf/pick_test_group_run_order.sh
@@ -2,13 +2,6 @@
 
 set -eu
 
-# Clone kibana repo from git reference
-echo "--- Clone kibana repo and chdir"
-git clone --reference /var/lib/gitmirrors/https---github-com-elastic-kibana-git https://github.com/elastic/kibana.git
-
-# TODO: checkout branch
-
-echo "--- Source env and utils from kibana .buildkite directory"
 source .buildkite/scripts/common/util.sh
 source .buildkite/scripts/common/env.sh
 
