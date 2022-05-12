6404939141e8ef4a7d0c26c92d8fcc6c585e828c
liza-mae <liza.dayoub@elastic.co>
2022-05-12 15:39:04 -0600

    Swallow error

--- .buildkite/pipelines/estf/pick_test_group_run_order.sh
+++ .buildkite/pipelines/estf/pick_test_group_run_order.sh
@@ -13,7 +13,7 @@ source .buildkite/scripts/common/util.sh
 source .buildkite/scripts/common/env.sh
 
 echo '--- Pick Test Group Run Order'
-node "$(dirname "${0}")/pick_test_group_run_order.js"
+node "$(dirname "${0}")/pick_test_group_run_order.js" || true
 
 echo '-- Upload test steps'
 source .buildkite/scripts/steps/estf/cloud/ftr_steps.sh
