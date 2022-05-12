b7c748a27a8bd055ec837b797c19b2c05cac6a7d
liza-mae <liza.dayoub@elastic.co>
2022-05-12 15:33:05 -0600

    Try steps

---
+++ .buildkite/scripts/steps/estf/cloud/ftr_steps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------------
+# Buildkite dynamic pipeline script for cloud kibana functional tests
+#
+# Author: Liza Dayoub
+# ----------------------------------------------------------------------------
+
+set -eu
+
+buildkite-agent artifact download ftr_run_order.json .
+group_count=$(jq -r '.count' ftr_run_order.json)
+
+echo "steps:"
+for i in $(seq -s ' ' 0 $((group_count-1))); do
+  configs=$(jq -r ".groups[$i].names | .[]" ftr_run_order.json)
+  echo "- label: Test $i"
+  echo "  command: echo $configs"
+  echo "  agents:"
+  echo "    queue: kibana-default"
+done
