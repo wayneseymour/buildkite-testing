a9ea9aac81f502b7e50bf1a12f9e88f6b7dd5425
liza-mae <liza.dayoub@elastic.co>
2022-05-12 16:47:24 -0600

    Upload commands

--- .buildkite/pipelines/estf/pick_test_group_run_order.sh
+++ .buildkite/pipelines/estf/pick_test_group_run_order.sh
@@ -13,3 +13,6 @@ source .buildkite/scripts/common/env.sh
 
 echo '--- Pick Test Group Run Order'
 node "$(dirname "${0}")/pick_test_group_run_order.js" || true
+
+echo '-- Upload Commands'
+.buildkite/scripts/steps/estf/cloud/ftr_steps.sh | buildkite-agent pipeline upload
--- .buildkite/scripts/steps/estf/cloud/ftr_steps.sh
+++ .buildkite/scripts/steps/estf/cloud/ftr_steps.sh
@@ -11,7 +11,6 @@ set -eu
 buildkite-agent artifact download ftr_run_order.json .
 group_count=$(jq -r '.count' ftr_run_order.json)
 
-echo "steps:"
 for i in $(seq -s ' ' 0 $((group_count-1))); do
   configs=$(jq -r ".groups[$i].names | .[]" ftr_run_order.json)
   echo "  - label: \"Test $i\""
