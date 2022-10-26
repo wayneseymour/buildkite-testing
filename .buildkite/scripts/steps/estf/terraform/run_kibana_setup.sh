ae9e5eef21493b52073be2ba130674ce630c5b16
liza-mae <liza.dayoub@elastic.co>
2022-10-26 17:43:28 -0600

    Add kibana setup

---
+++ .buildkite/scripts/steps/estf/terraform/run_kibana_setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------------------------------
+# Buildkite script to run kibana operating system tests
+#
+# Author: Liza Dayoub
+# ----------------------------------------------------------------------------
+
+set -euo pipefail
+
+source .buildkite/scripts/steps/estf/ansible/ansible_setup.sh
+
+echo "--- Run kibana operating system tests"
+export ESTF_PUBLIC_IP=$(buildkite-agent meta-data get "estf-tf-ip-$ESTF_META_ID")
+./playbooks/kibana/ci/buildkite_os_testing.sh
+
+OUTPUT_FILE="/tmp/$ESTF_META_ID"
+
+read user < $OUTPUT_FILE ESTF_DEPLOYMENT_USERNAME
+read password < $OUTPUT_FILE ESTF_DEPLOYMENT_PASSWORD
+read kibana_url < $OUTPUT_FILE ESTF_KIBANA_URL
+read elasticsearch_url < $OUTPUT_FILE ESTF_ELASTICSEARCH_URL
+
+ESTF_KIBANA_HASH=$(curl -s -u "$ESTF_DEPLOYMENT_USERNAME:$ESTF_DEPLOYMENT_PASSWORD" $ESTF_KIBANA_URL/api/status | jq -r .version.build_hash)
+
+buildkite-agent meta-data set "estf-kibana-hash-$ESTF_META_ID" $ESTF_KIBANA_HASH
+buildkite-agent meta-data set "estf-elasticsearch-url-$ESTF_META_ID" $ESTF_ELASTICSEARCH_URL
+buildkite-agent meta-data set "estf-kibana-url-$ESTF_META_ID" $ESTF_KIBANA_URL
+buildkite-agent meta-data set "estf-deployment-password-$ESTF_META_ID" $ESTF_DEPLOYMENT_PASSWORD
