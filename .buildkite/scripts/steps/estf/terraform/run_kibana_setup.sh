76756048d2b5c6fbcb97c1ce6c8c1c62e680677f
liza-mae <liza.dayoub@elastic.co>
2022-10-27 08:05:29 -0600

    More debug

--- .buildkite/scripts/steps/estf/terraform/run_kibana_setup.sh
+++ .buildkite/scripts/steps/estf/terraform/run_kibana_setup.sh
@@ -14,20 +14,27 @@ echo "--- Run kibana operating system tests"
 export ESTF_PUBLIC_IP=$(buildkite-agent meta-data get "estf-tf-ip-$ESTF_META_ID")
 ./playbooks/kibana/ci/buildkite_os_testing.sh
 
-echo "Debug after kibana playbook"
 OUTPUT_FILE="/tmp/$ESTF_META_ID"
 
-echo "--Output file: $OUTPUT_FILE"
+echo "--- Output file is: $OUTPUT_FILE"
 ls -l $OUTPUT_FILE
 whoami
+cat $OUTPUT_FILE
 
-echo "-- Read vars"
+echo "--- Read vars"
 read user < $OUTPUT_FILE ESTF_DEPLOYMENT_USERNAME
 read password < $OUTPUT_FILE ESTF_DEPLOYMENT_PASSWORD
 read kibana_url < $OUTPUT_FILE ESTF_KIBANA_URL
 read elasticsearch_url < $OUTPUT_FILE ESTF_ELASTICSEARCH_URL
 
-echo "-- Get kibana hash"
+echo $ESTF_DEPLOYMENT_USERNAME
+echo $ESTF_DEPLOYMENT_PASSWORD
+echo $ESTF_KIBANA_URL
+echo $ESTF_ELASTICSEARCH_URL
+
+sleep 600
+
+echo "--- Get kibana hash"
 ESTF_KIBANA_HASH=$(curl -s -u "$ESTF_DEPLOYMENT_USERNAME:$ESTF_DEPLOYMENT_PASSWORD" $ESTF_KIBANA_URL/api/status | jq -r .version.build_hash)
 
 echo "--- Set metadata"
@@ -36,5 +43,5 @@ buildkite-agent meta-data set "estf-elasticsearch-url-$ESTF_META_ID" $ESTF_ELAST
 buildkite-agent meta-data set "estf-kibana-url-$ESTF_META_ID" $ESTF_KIBANA_URL
 buildkite-agent meta-data set "estf-deployment-password-$ESTF_META_ID" $ESTF_DEPLOYMENT_PASSWORD
 
-echo "-- chdir uplevel"
+echo "--- Chdir uplevel"
 cd ../
