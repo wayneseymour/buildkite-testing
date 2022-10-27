44451c2d5c994f2c3665afe176fa46dedda92946
liza-mae <liza.dayoub@elastic.co>
2022-10-27 09:19:34 -0600

    Fix reading of vars

--- .buildkite/scripts/steps/estf/terraform/run_kibana_setup.sh
+++ .buildkite/scripts/steps/estf/terraform/run_kibana_setup.sh
@@ -17,7 +17,7 @@ export ESTF_PUBLIC_IP=$(buildkite-agent meta-data get "estf-tf-ip-$ESTF_META_ID"
 OUTPUT_FILE="/tmp/$ESTF_META_ID"
 
 echo "--- Read instance file vars"
-while IFS=":" read -r key value
+while IFS=": " read -r key value
 do
   case $key in
     user)
@@ -37,27 +37,14 @@ do
   esac
 done < "$OUTPUT_FILE"
 
-echo "DEBUG INFO:"
-echo $ESTF_META_ID
-echo $ESTF_DEPLOYMENT_USERNAME
-echo $ESTF_DEPLOYMENT_PASSWORD
-echo $ESTF_KIBANA_URL
-echo $ESTF_ELASTICSEARCH_URL
-echo "curl command $ESTF_DEPLOYMENT_USERNAME:$ESTF_DEPLOYMENT_PASSWORD $ESTF_KIBANA_URL/api/status"
-
 echo "--- Get kibana hash"
 ESTF_KIBANA_HASH=$(curl --insecure -s -u "$ESTF_DEPLOYMENT_USERNAME:$ESTF_DEPLOYMENT_PASSWORD" $ESTF_KIBANA_URL/api/status | jq -r .version.build_hash)
-
-echo $ESTF_KIBANA_HASH
 
 echo "--- Set metadata"
 buildkite-agent meta-data set "estf-kibana-hash-$ESTF_META_ID" $ESTF_KIBANA_HASH
 buildkite-agent meta-data set "estf-elasticsearch-url-$ESTF_META_ID" $ESTF_ELASTICSEARCH_URL
 buildkite-agent meta-data set "estf-kibana-url-$ESTF_META_ID" $ESTF_KIBANA_URL
 buildkite-agent meta-data set "estf-deployment-password-$ESTF_META_ID" $ESTF_DEPLOYMENT_PASSWORD
-
-echo $(buildkite-agent meta-data get "estf-kibana-hash-$ESTF_META_ID")
-sleep 600
 
 echo "--- Chdir uplevel"
 cd ../
