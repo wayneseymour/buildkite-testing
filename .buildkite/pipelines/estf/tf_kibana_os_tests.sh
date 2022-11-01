ceb876413791c05b5fd7cc5d65838aa76498fbe8
liza-mae <liza.dayoub@elastic.co>
2022-11-01 12:26:56 -0600

    Add dynamic pipeline

---
+++ .buildkite/pipelines/estf/tf_kibana_os_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------------
+# Buildkite dynamic pipeline script for platform testing
+#
+# Author: Liza Dayoub
+# ----------------------------------------------------------------------------
+
+set -euo pipefail
+
+echo "  - command: .buildkite/scripts/lifecycle/pre_build.sh"
+echo "    label: Pre-Build"
+echo "    timeout_in_minutes: 10"
+echo "    agents:"
+echo "      queue: kibana-default"
+echo "  - wait"
+
+osFile=".buildkite/scripts/steps/estf/terraform/testing/operating_systems"
+while read line; do
+  if [[ -z "$line" ]] || [[ "$line" =~ ^# ]]; then
+    continue
+  fi
+  read -a osarray <<< $line
+  if [[ "${#osarray[@]}" -ne 3 ]]; then
+    continue
+  fi
+  echo "  - command: .buildkite/scripts/steps/estf/terraform/tf_kibana_os_testing.sh"
+  echo "    label: ${osarray[2]}"
+  echo "    agents:"
+  echo "      queue: n2-4"
+  echo "    env:"
+  echo "      ESTF_META_ID: tf-kibana-os-${osarray[2]}"
+  echo "      ESTF_KIBANA_OS_TEST: true"
+  echo "      AIT_PROVIDER: ${osarray[0]}"
+  echo "      AIT_USER: buildkite-agent"
+  echo "      AIT_IMAGE: ${osarray[1]}/${osarray[2]}"
+done < "$osFile"
+echo "  - wait: ~"
+echo "    continue_on_failure: true"
+echo "  - command: .buildkite/scripts/lifecycle/post_build.sh"
+echo "    label: Post-Build"
+echo "    agents:"
+echo "      queue: kibana-default"
